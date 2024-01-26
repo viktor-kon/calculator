387e05f0fb6a21de6a61459d2e06c4a04bfefed9
Viktor-Kon <65969014+viktor-kon@users.noreply.github.com>
2024-01-26 06:17:29 +0300

    Add files via upload

---
+++ all/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Calculator from './components/Calculator.vue';
+</script>
+
+<template>
+<div class="container">
+  <Calculator />
+</div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
---
+++ all/src/components/CalculatorDisplay.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+    value: String,
+});
+</script>
+
+<template>
+<p class="display">{{ value }}</p>
+</template>
+
+<style scoped>
+.display {
+    margin: 0;
+    text-align: right;
+    padding: 5px 10px;
+    font-size: 30px;
+    border: 1px solid #2c3e50;
+    border-radius: 14px;
+}
+</style>
